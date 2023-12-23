8e056e7a48452142f0bc979f2cc5637c21fe7e04
Ben Boyle <me@benboyle.ca>
2023-12-23 14:17:01 -0400

    finished homework up to memory movs

--- mySim86/main.c
+++ mySim86/main.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 #include "../computer_enhance/perfaware/sim86/shared/sim86_shared.h"
 #pragma comment (lib, "sim86_shared_debug.lib")
@@ -21,9 +22,14 @@ u16 proc_flags = 0;
 #define FLAG_BIT_ZERO 0x1
 #define FLAG_BIT_SIGN 0x2
 
-void execute_instruction(instruction inst);
+u32 proc_registers[9] = {-1,0,0,0,0,0,0,0, 0};
+u32 proc_inst_ptr = 0;
 
-u32 proc_registers[8] = {0,0,0,0,0,0,0,0};
+u8 proc_memory[65536];
+
+void gen_flags_str(char* _out, u16 flags);
+u16 get_data(instruction inst);
+void execute_instruction(instruction inst);
 
 int main(int argc, char **argv)
 {
@@ -66,13 +72,15 @@ int main(int argc, char **argv)
         } while ((c = getc(bin_file)) != EOF);
     }
 
+    // zero 8086 simulated memory
+    memset(&proc_memory, 0, 65536);
+
     // Decode the instructions
-    u32 Offset = 0;
-    while (Offset < instruction_byte_count) {
+    while (proc_inst_ptr < instruction_byte_count) {
         instruction Decoded;
-        Sim86_Decode8086Instruction(instruction_byte_count - Offset, instruction_bytes + Offset, &Decoded);
+        Sim86_Decode8086Instruction(instruction_byte_count - proc_inst_ptr, instruction_bytes + proc_inst_ptr, &Decoded);
         if (Decoded.Op) {
-            Offset += Decoded.Size;
+            proc_inst_ptr += Decoded.Size;
             Sim86_PrintInstruction(Decoded, stdout);
 
             IF_DEBUG {
@@ -88,19 +96,24 @@ int main(int argc, char **argv)
         }
     }
 
-    // TODO: omit registers that were not touched
     printf("\n=====================================\nREGISTERS AT END:\n");
     register_access dummy_reg = {0, 0, 2};
 
+    // print values of 'regular' registers
     for (int i = 1; i <= 8; ++i) {
-        dummy_reg.Index = i;
-        printf("\t%s: 0x%.4x (%d)\n", Sim86_RegisterNameFromOperand(&dummy_reg), proc_registers[i-1], proc_registers[i-1]);
+        if (proc_registers[i] != 0) {
+            dummy_reg.Index = i;
+            printf("\t%s: 0x%.4x (%d)\n", Sim86_RegisterNameFromOperand(&dummy_reg), proc_registers[i], proc_registers[i]);
+        }
     }
 
-    printf("\tflags: %s%s\n",
-        proc_flags & FLAG_BIT_SIGN ? "S" : "",
-        proc_flags & FLAG_BIT_ZERO ? "Z" : ""
-    );
+    // print instruction pointer value
+    printf("    ip: 0x%.4x (%d)\n", proc_inst_ptr, proc_inst_ptr);
+
+    // print active flags in flag "register"
+    char flags_out_str[16] = "";
+    gen_flags_str(flags_out_str, proc_flags);
+    printf("    flags: %s\n", flags_out_str);
 
     // Close files
     if (bin_file != stdin && bin_file != NULL) {
@@ -108,32 +121,69 @@ int main(int argc, char **argv)
     }
 }
 
-u32 *get_lhs_reg(instruction inst) {
-    return &proc_registers[inst.Operands[0].Register.Index - 1];
+void gen_flags_str(char* _out, u16 flags) {
+    sprintf(_out, "%s%s",
+        flags & FLAG_BIT_SIGN ? "S" : "",
+        flags & FLAG_BIT_ZERO ? "Z" : ""
+    );
+}
+
+u16 get_mem_index(instruction inst, u8 operand_index) {
+    assert(inst.Operands[operand_index].Type == Operand_Memory);
+    effective_address_expression addr = inst.Operands[operand_index].Address;
+
+    u16 mem_index = addr.Displacement;
+
+    u8 term0 = addr.Terms[0].Register.Index;
+    if (term0 >= 1 && term0 <= 8) {
+        mem_index += proc_registers[addr.Terms[0].Register.Index];
+    }
+
+    u8 term1 = addr.Terms[1].Register.Index;
+    if (term1 >= 1 && term1 <= 8) {
+        mem_index += proc_registers[addr.Terms[1].Register.Index];
+    }
+
+    assert(mem_index < sizeof(proc_memory) - 1);
+    return mem_index;
 }
 
 u16 get_data(instruction inst) {
-    return inst.Operands[1].Type == Operand_Immediate
-        ? inst.Operands[1].Immediate.Value
-        : proc_registers[inst.Operands[1].Register.Index - 1];
+    switch (inst.Operands[1].Type) {
+        case Operand_Immediate:
+            return inst.Operands[1].Immediate.Value;
+            break;
+        case Operand_Register:
+            return proc_registers[inst.Operands[1].Register.Index];
+            break;
+        case Operand_Memory:
+            return proc_memory[get_mem_index(inst, 1)];;
+            break;
+        case Operand_None:
+        default:
+            return 0;
+    }
 }
 
 void exec_mov(instruction inst) {
-    assert(inst.Operands[0].Register.Index > 0 &&
-            inst.Operands[0].Register.Index <= 8);
-
-    proc_registers[inst.Operands[0].Register.Index - 1] = get_data(inst);
+    if (inst.Operands[0].Type == Operand_Register) {
+        assert(inst.Operands[0].Register.Index >= 1 && inst.Operands[0].Register.Index <= 8);
+        proc_registers[inst.Operands[0].Register.Index] = get_data(inst);
+    } else if (inst.Operands[0].Type == Operand_Memory) {
+        u16 mem_index = get_mem_index(inst, 0);
+        proc_memory[mem_index] = get_data(inst);
+    }
 }
 
 void set_flags(u16 result) {
     proc_flags = 0;
 
-    proc_flags |= (FLAG_BIT_ZERO && (result == 0));
-    proc_flags |= (FLAG_BIT_SIGN && (result & 0x8000));
+    proc_flags |= FLAG_BIT_ZERO * (result == 0);
+    proc_flags |= FLAG_BIT_SIGN * !!(result & 0x8000);
 }
 
 void exec_arith(instruction inst) {
-    u32 *lhs_reg = get_lhs_reg(inst);
+    u32 *lhs_reg = &proc_registers[inst.Operands[0].Register.Index];
     u16 data = get_data(inst);
     u16 result = 0;
 
@@ -158,16 +208,22 @@ void exec_arith(instruction inst) {
 }
 
 void execute_instruction(instruction inst) {
-    u32 *lhs_reg = get_lhs_reg(inst);
+    u32 init_inst_ptr = proc_inst_ptr;
+    u16 init_flags = proc_flags;
 
+    u32 *lhs_reg = &proc_registers[0];
+    u32 init_reg = 0;
     if (inst.Operands[0].Type == Operand_Register) {
-        // print initial value of register
-        printf(" ; %s:0x%x->",
-            Sim86_RegisterNameFromOperand(&inst.Operands[0].Register),
-            *lhs_reg);
+        lhs_reg = &proc_registers[inst.Operands[0].Register.Index];
+        init_reg = *lhs_reg;
     }
 
     switch (inst.Op) {
+        case Op_jne:
+            if (!(proc_flags & FLAG_BIT_ZERO)) {
+                proc_inst_ptr += inst.Operands[0].Immediate.Value;
+            }
+            break;
         case Op_mov:
             exec_mov(inst);
             break;
@@ -179,8 +235,23 @@ void execute_instruction(instruction inst) {
         break;
     }
 
-    if (inst.Operands[0].Type == Operand_Register) {
-        // print resulting value of register
-        printf("0x%x", *lhs_reg);
+    { // print out global effects of executing this instruction
+        char reg_out_str[32];
+        if (inst.Operands[0].Type == Operand_Register) {
+            sprintf(reg_out_str, " ; %s:0x%x->0x%x",
+                Sim86_RegisterNameFromOperand(&inst.Operands[0].Register),
+                init_reg, *lhs_reg);
+        }
+
+        char flags_out_str[32] = "";
+        if (proc_flags != init_flags) {
+            char flags_1[16] = "";
+            char flags_2[16] = "";
+            gen_flags_str(flags_1, init_flags);
+            gen_flags_str(flags_2, proc_flags);
+            sprintf(flags_out_str, "flags:%s->%s", flags_1, flags_2);
+        }
+
+        printf("%s ip:0x%x->0x%x %s", reg_out_str, init_inst_ptr, proc_inst_ptr, flags_out_str);
     }
 }
